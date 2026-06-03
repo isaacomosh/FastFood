ccb845af066637a050b998f9e5410a7eb39bdb0b
iceharc <isaacelly85@gmail.com>
2026-06-03 08:56:35 +0300

    Added loading screen and hmepage.dart

---
+++ chicky/lib/loading_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Image.asset('assets/loading_screen.png'),
+      ),
+    );
+  }
+}
